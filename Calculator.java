9d27119d5862006f3e9f0e180f79f03da8c7d404
Sunilbabu09 <battusunilbabu@gmail.com>
2017-01-31 11:01:16 -0600

    first commit

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+//import java.util.Scanner;
+public class Calculator {
+public static void main(String[] args){
+	
+	//Scanner ab = new Scanner(System.in);
+	double fn=20,sn=10;
+	double total = fn+sn;
+        System.out.println("additon is :" +total);
+
+
+	//System.out.println("Enter your first number");
+	//fn = ab.nextDouble();
+	//System.out.println("Enter your Second number");
+	//sn = ab.nextDouble();
+	
+	//System.out.println("Enter your option ");
+	//System.out.println("1.Addition	2.Subtraction	3.Multiply	4.Division" +option  );
+	//option = ab.nextInt();	
+	
+	//switch(option ){
+	//case 1:System.out.println(+(fn+sn));break;
+	//case 2:System.out.println(+(fn-sn));break;
+	//case 3:System.out.println(+(fn*sn));break;
+	//case 4:System.out.println(+(fn/sn));break;
+	//default:System.out.println("Wrong option");
+	
+	//}
+}
+}
